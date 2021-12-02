81fe2ab8ff8cf58104965f772d6cab8caac41588
evgenii-burlakov <esburlakov94@gmail.com>
2021-12-02 15:23:35 +0300

    HW01 fix

--- src/main/java/ru/otus/spring/dao/QuestionDaoInFile.java
+++ src/main/java/ru/otus/spring/dao/QuestionDaoInFile.java
@@ -14,7 +14,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionDaoInFile implements QuestionDao {
+public class QuestionDaoInFile implements QuestionService {
     private final String questionFile;
 
     public QuestionDaoInFile(String questionFile) {
