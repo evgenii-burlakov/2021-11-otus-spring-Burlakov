ffc35e72420d96d1ad12ee84a11c38f9572e675b
evgenii-burlakov <esburlakov94@gmail.com>
2022-01-09 00:28:13 +0300

    fix Main class

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -2,14 +2,10 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.otus.spring.service.test.TestingService;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
-        TestingService service = context.getBean(TestingService.class);
-        service.printTest();
+        SpringApplication.run(Main.class, args);
     }
 }
