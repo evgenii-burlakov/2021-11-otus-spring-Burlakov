4cf242d0f7f51b0db4c75190407286edfbb5a9cb
evgenii-burlakov <esburlakov94@gmail.com>
2022-02-16 14:19:11 +0300

    migrate books and genres web pages to js

--- library-application/src/main/java/ru/otus/libraryapplication/controller/genre/GenreController.java
+++ library-application/src/main/java/ru/otus/libraryapplication/controller/genre/GenreController.java
@@ -2,58 +2,48 @@ package ru.otus.libraryapplication.controller.genre;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.libraryapplication.dto.GenreDto;
 import ru.otus.libraryapplication.service.genre.GenreService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class GenreController {
     private final GenreService genreService;
 
     @GetMapping("/genres")
-    public String getAllGenres(Model model) {
-        List<GenreDto> genres = genreService.getAll().stream()
+    public List<GenreDto> getAllGenres(Model model) {
+        return genreService.getAll().stream()
                 .map(GenreDto::toDto)
                 .collect(Collectors.toList());
-        model.addAttribute("genres", genres);
-        return "genres";
     }
 
-    @PostMapping("/genres/delete")
-    public String deleteGenreById(@RequestParam("id") long id) {
+    @DeleteMapping("/genres")
+    public ResponseEntity deleteGenreById(@RequestParam("id") long id) {
         genreService.deleteById(id);
-        return "redirect:/genres";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/genres/edit")
-    public String editPage(@RequestParam("id") Long id, Model model) {
-        GenreDto genre = GenreDto.toDto(genreService.getById(id));
-        model.addAttribute("genre", genre);
-        return "editGenre";
+    @GetMapping("/genres/{id}")
+    public GenreDto editGenre(@PathVariable("id") Long id) {
+        return GenreDto.toDto(genreService.getById(id));
     }
 
-    @PostMapping("/genres/edit")
-    public String updateGenre(GenreDto genre) {
+    @PatchMapping("/genres/{id}")
+    public ResponseEntity updateGenre(GenreDto genre) {
         genreService.update(genre.getId(), genre.getName());
-        return "redirect:/genres";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/genres/create")
-    public String createPage() {
-        return "createGenre";
-    }
-
-    @PostMapping("/genres/create")
-    public String createGenre(GenreDto genre) {
+    @PostMapping("/genres")
+    public ResponseEntity createGenre(GenreDto genre) {
         genreService.create(genre.getName());
-        return "redirect:/genres";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
