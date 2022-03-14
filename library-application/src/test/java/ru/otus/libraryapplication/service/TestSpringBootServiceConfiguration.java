3abf9aa211872a42e725ad77ff102611229bc8f2
evgenii-burlakov <esburlakov94@gmail.com>
2022-03-14 21:09:13 +0300

    small refactor

--- library-application/src/test/java/ru/otus/libraryapplication/service/TestSpringBootServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.libraryapplication.service;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootConfiguration
-@ComponentScan("ru.otus.libraryapplication.service")
-public class TestSpringBootServiceConfiguration {
-}
